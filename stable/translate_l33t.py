f8b5e829a9b894b4c09cb4d19550435358a577b8
Robert-Karl <robert.karl@colorado.edu>
2017-12-14 09:34:55 -0700

    Changed translation to run in a for loop. Cleaned up syntax. Wait for user input to close.

--- stable/translate_l33t.py
+++ stable/translate_l33t.py
@@ -3,20 +3,37 @@
 Created on Mon Nov 20 15:07:31 2017
 
 This function takes a user input and turns it into L33T 5P34K
+It does this by replacing lowercase letters with appropriate numerals
 
-@author: rinfi_000
+This is the assignment key:
+o -> 0
+l -> 1
+NONE -> 2
+e -> 3
+a -> 4
+s -> 5
+NONE -> 6
+t -> 7
+b -> 8
+NONE -> 9
+
+@author: robert-karl
 """
-print("Please type phrase to be translated: ")
-phrase = input()
+request = "Please type phrase to be translated: \n"
+phrase = input(request)
+
+# Assemble the key
+oldChar = 'oleastb'
+newChar = '0134578'
 
 # Now we will replace various lower case letters
-phrase = phrase.replace('a','4')
-phrase = phrase.replace('e','3')
-phrase = phrase.replace('o','0')
-phrase = phrase.replace('l','1')
-phrase = phrase.replace('s','5')
-phrase = phrase.replace('t','7')
-phrase = phrase.replace('b','8')
+for nn in range(0,len(oldChar)):
+    phrase = phrase.replace(oldChar[nn],newChar[nn])
+
 
 # output the final result
+print('\n')
 print(phrase)
+# Let the user determine when the program ends.
+exitPrompt = 'Press <ENTER> to exit.'
+input(exitPrompt)
