49b971115eb6e10454859db0ad0e1a5187c60527
Robert-Karl <robert.karl@colorado.edu>
2017-12-21 13:04:57 -0700

    Added ChooseWord function for selecting the random words

--- stable/poetry.py
+++ stable/poetry.py
@@ -8,16 +8,33 @@ It's very deep and a little bit edgy.
 @author: robert-karl
 """
 
-
+def ChooseWord(choices,Nword,posLabel,posVocab):
+    """
+    Chooses a word of a given part of speech, then removes that word from the dictionary.
+    Inputs:
+        choices     The dict of selected words
+        Nword       The number of words of that part of speech to take
+        posLabel    Label for tis part of speech
+        posVocab    The array of words for that part of speech
+    Returns:
+        choices     The updated dict of selected words
+    """
+    from random import choice
+    for word in range(0,Nword):
+        # Pick a word
+        newWord = choice(posVocab)
+        # Add the word to the choices dict
+        choices[posLabel+str(word)]=newWord
+        # Remove the word so that it will not be selected twice
+        posVocab.remove(newWord)
+    return choices
 
 def makePoem():
-    from random import choice
-
     # Poetic structure
     structure = "{art0} {adj0} {noun0} \n\n{art0} {adj0} {noun0} {verb0} {pre0} the {adj1} {noun1}\n{adv0}, the {noun0} {verb1} the {noun1} \n{verb1} {pre1} {art1} {adj2} {noun2}."
     
     # Build the vocabulary
-    adjectives = ['furry','balding','incredulous','fragrant','exuberant','glistening']
+    adjectives = ['furry','balding','incredulous','fragrant','exuberant','glistening','hopeful']
     nouns = ['fossil','horse','aardvark','judge','chef','mango','extrovert','gorilla']
     verbs = ['kicks','jingles','bounces','slurps','meows','explodes','curdles']
     adverbs = ['curiously','extravagantly','tantalizingly','furiously','sensuously']
@@ -25,50 +42,26 @@ def makePoem():
     
     # Select the words, put them in the choices dictionary
     choices = {}
-    # For the structure above we need
-    
-    # 3 adjectives
+    # Select the words that we need
     Nadj = 3
-    for adj in range(0,Nadj):
-        newAdj = choice(adjectives)
-        choices['adj'+str(adj)]=newAdj
-        adjectives.remove(newAdj)
-    
-    # 3 nouns
+    choices = ChooseWord(choices,Nadj,'adj',adjectives)
     Nn = 3
-    for nn in range(0,Nn):
-        newNoun = choice(nouns)
-        choices['noun'+str(nn)]=newNoun
-        nouns.remove(newNoun)
-    
-    # 2 verbs
+    choices = ChooseWord(choices,Nn,'noun',nouns)
     Nv = 2
-    for vv in range(0,Nv):
-        newVerb = choice(verbs)
-        choices['verb'+str(vv)]=newVerb
-        verbs.remove(newVerb)
-    
-    # 1 adverb
+    choices = ChooseWord(choices,Nv,'verb',verbs)
     Nadv = 1
-    for adv in range(0,Nadv):
-        newAdv = choice(adverbs)
-        choices['adv'+str(adv)]=newAdv
-        adverbs.remove(newAdv)
-    
-    # 2 prepositions
+    choices = ChooseWord(choices,Nadv,'adv',adverbs)
     Np = 2
-    for pp in range(0,Np):
-        newPrep = choice(prepositions)
-        choices['pre'+str(pp)]=newPrep
-        prepositions.remove(newPrep)
+    choices = ChooseWord(choices,Np,'pre',prepositions)
         
     # Set the articles based on the following word
-    vowels = 'aeiou'
-    if vowels.find(choices['adj0'])>=0:
+    # These are the letters that force a preceeding artice to be an, not a
+    vowels = 'aeiouh'
+    if vowels.find(choices['adj0'][0])>=0:
         choices['art0'] = 'an'
     else:
         choices['art0'] = 'a'
-    if vowels.find(choices['adj2'])>=0:
+    if vowels.find(choices['adj2'][0])>=0:
         choices['art1'] = 'an'
     else:
         choices['art1'] = 'a'
